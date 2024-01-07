b93dd8efb26258eb2c107b27b08e6ef70d92ad21
wostrau <a.ostrovtsov@gmail.com>
2024-01-07 16:42:33 +0100

    added ProjectsSidebar, NewProject, Input components

---
+++ src/components/NewProject.jsx
@@ -0,0 +1,24 @@
+import Input from './Input';
+
+export default function NewProject() {
+  return (
+    <div>
+      <menu>
+        <li>
+          <button>Cancel</button>
+        </li>
+        <li>
+          <button>Save</button>
+        </li>
+      </menu>
+      <div>
+        <Input label='Title' />
+        <Input
+          label='Description'
+          textarea
+        />
+        <Input label='Due Date' />
+      </div>
+    </div>
+  );
+}
