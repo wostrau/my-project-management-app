6418fd2c8bf9fccc9c1430f3e61a8cba7e256523
wostrau <a.ostrovtsov@gmail.com>
2024-01-07 18:55:17 +0100

    added new components: Button, NoProjectSelected / adjusted previous ones

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,56 @@
+import { useState } from 'react';
+import NoProjectSelected from './components/NoProjectSelected';
 import ProjectsSidebar from './components/ProjectsSidebar';
+import NewProject from './components/NewProject';
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  function handleAddProject(projectData) {
+    setProjectsState((prevState) => {
+      const projectId = Math.random();
+      const newProject = {
+        ...projectData,
+        id: projectId,
+      };
+
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+
+  // console.log(projectsState);
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
-    <main className='h-screen my-8'>
-      <ProjectsSidebar />
+    <main className='h-screen my-8 flex gap-8'>
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
+      {content}
     </main>
   );
 }
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,29 @@
-export default function Input({ label, textarea, ...props }) {
+import { forwardRef } from 'react';
+
+const Input = forwardRef(function Input({ label, textarea, ...props }, ref) {
+  const inputClasses =
+    'w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600';
+
   return (
-    <p>
-      <label>{label}</label>
-      {textarea ? <textarea {...props} /> : <input {...props} />}
+    <p className='flex flex-col gap-1 my-4'>
+      <label className='text-sm font-bold uppercase text-stone-500'>
+        {label}
+      </label>
+      {textarea ? (
+        <textarea
+          ref={ref}
+          className={inputClasses}
+          {...props}
+        />
+      ) : (
+        <input
+          ref={ref}
+          className={inputClasses}
+          {...props}
+        />
+      )}
     </p>
   );
-}
+});
+
+export default Input;
